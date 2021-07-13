68dd5109cec2fb8bb29181fc1d03d07230b87b5a
JekoSantiago <86212426+JekoSantiago@users.noreply.github.com>
2021-07-13 09:16:19 +0800

    Performance Fix

--- resources/views/layouts/components/header_css.blade.php
+++ resources/views/layouts/components/header_css.blade.php
@@ -6,9 +6,9 @@
 @yield('css_plugins')
 
 <!-- Global Stylesheet -->
-<link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('assets/css/icons.min.css')}}" rel="preload stylesheet" as="font" type="text/css" />
 <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
-<link href="{{asset('assets/css/app.min.css')}} " rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+<link href="{{asset('assets/css/app.min.css')}} " rel="preload stylesheet" as="font" type="text/css" id="app-default-stylesheet" />
 
 <!-- Custom Stylesheet -->
 <link href="{{asset('assets/css/style.css')}} " rel="stylesheet" type="text/css"/>
